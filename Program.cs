7236ef59fb74715c580b1bae187c31e650b6264c
Adis Alisic <22AAlisic@heckgrammar.co.uk>
2025-01-07 10:02:15 +0000

    ggg

--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
-﻿namespace ConsoleApp1
+﻿using System.ComponentModel.Design;
+
+namespace ConsoleApp1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+            // selection
+            int age;
+            Console.WriteLine("What is your age?");
+            age = Convert.ToInt16(Console.ReadLine());
+            if (age > 16)
+            {
+                Console.WriteLine("you don't need to be in school!");
+            }
+            else if (Age > 21)
+            {
+                Console.WriteLine("you don't need to be in education!");
+
+            }
